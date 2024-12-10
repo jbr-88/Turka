a23de8258b8fe3cad0a29e91d65ecbd7db0e6b15
jbr-88 <braza08.88@gmail.com>
2024-12-10 18:46:25 +0100

    First player animations done

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEditor.Tilemaps;
 
 public class PlayerInput : MonoBehaviour
 {
@@ -14,17 +15,32 @@ public class PlayerInput : MonoBehaviour
     private float xInput;
     private bool jumpInput;
     private bool iAmOnTheGround;
+    private bool facingRight;
+    private Animator anim;
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         movement = new Vector2();
         iAmOnTheGround = false;
+        anim = GetComponent<Animator>();
+        facingRight = true;
     }
 
     void Update()
     {
         xInput = Input.GetAxis("Horizontal");
         jumpInput = Input.GetKey(KeyCode.Space);
+
+        if((xInput < 0) && (facingRight)) {
+            Flip();
+            facingRight = false;
+        }else if((xInput > 0) && (!facingRight)) {
+            Flip();
+            facingRight = true;
+        }
+
+        anim.SetFloat("xSpeed", Mathf.Abs(body.linearVelocityX));
+        anim.SetFloat("ySpeed", Mathf.Abs(body.linearVelocityY));
         
         if(Physics2D.OverlapCircle(groundCheckPoint.position, 0.02f, whatIsGround)) {
             iAmOnTheGround = true;
@@ -49,5 +65,12 @@ public class PlayerInput : MonoBehaviour
         }
         */
         body.linearVelocity = movement;
+    }
+
+    void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= (-1);
+        transform.localScale = scale;
     }
 }
